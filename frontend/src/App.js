932bdba57d0acd1fdfefc4645d6073e64cc3c433
Maciej <hoffix0123@gmail.com>
2021-09-04 19:34:26 +0200

    project

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import Customers from "./components/Customers";
+function App() {
+  return (
+    <div className="App">
+      <Customers />
+    </div>
+  );
+}
+
+export default App;
