3c449151ff77289d12aa8556db0b1c5a566365d3
Maciej <hoffix0123@gmail.com>
2021-09-06 20:14:51 +0200

    Add basic config

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,23 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+// app.use(function (req, res, next) {
+//   next();
+// });
+
+const users = [];
+
 app.get("/api/customers", (req, res) => {
+  console.log(req.method);
+  console.log(req.params);
   const customers = [
     { id: 1, firstName: "John", lastName: "Doe" },
     { id: 2, firstName: "Brad", lastName: "Traversy" },
@@ -10,6 +25,26 @@ app.get("/api/customers", (req, res) => {
   ];
 
   res.json(customers);
+});
+
+app.post("/v1/users", (req, res) => {
+  console.log(req.method);
+  console.log(req.body);
+
+  users.push({
+    id: 1,
+    name: req.params.name,
+    password: req.params.password,
+  });
+
+  res.status(200).json({});
+});
+
+app.get("/v1/users/:id", (req, res) => {
+  console.log(req.params);
+  console.log(req.query);
+
+  res.status(200).json({});
 });
 
 const port = 5000;
