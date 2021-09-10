e2f801c4bcef93c1be566e943c55cd40ee08676b
Maciej <hoffix0123@gmail.com>
2021-09-10 20:31:13 +0200

    Basic app layout

--- backend/server.js
+++ backend/server.js
@@ -1,52 +1,25 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import router from "./routes/posts.js";
 const app = express();
 
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/posts", router);
 
-// parse application/json
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
-// app.use(function (req, res, next) {
-//   next();
-// });
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
 
-const users = [];
+app.use(cors());
 
-app.get("/api/customers", (req, res) => {
-  console.log(req.method);
-  console.log(req.params);
-  const customers = [
-    { id: 1, firstName: "John", lastName: "Doe" },
-    { id: 2, firstName: "Brad", lastName: "Traversy" },
-    { id: 3, firstName: "Mary", lastName: "Swanson" },
-  ];
+const CONNECTION_URL =
+  "mongodb+srv://hoffix123:lolz123@cluster0.jdu9c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const port = process.env.port || 5000;
 
-  res.json(customers);
-});
-
-app.post("/v1/users", (req, res) => {
-  console.log(req.method);
-  console.log(req.body);
-
-  users.push({
-    id: 1,
-    name: req.params.name,
-    password: req.params.password,
-  });
-
-  res.status(200).json({});
-});
-
-app.get("/v1/users/:id", (req, res) => {
-  console.log(req.params);
-  console.log(req.query);
-
-  res.status(200).json({});
-});
-
-const port = 5000;
-
-app.listen(port, () => `Server running on port ${port}`);
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(port, () => console.log(`Server running on port: ${port}`))
+  )
+  .catch((err) => console.log(err.message));
